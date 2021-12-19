b7c27e65a5b29213aa33c94f600c7c71fabbf0fd
Nosma2520 <nexiattiogbe@gmail.com>
2021-12-19 06:06:12 -0500

    Implemented oversampling and decimation to limit ADC quantization errror.

--- Concentration/conductance.ino
+++ Concentration/conductance.ino
@@ -1,3 +1,6 @@
+#include <Oversampling.h>
+
+Oversampling adc(10, 16, 2);
 void setup() 
 {
   Serial.begin(115200); //This code sets up the Serial port at 115200 baud rate
@@ -7,7 +10,7 @@ void setup()
 void loop() //This function loops while the Arduino is powered
 {
   // Calculations of voltage and conductance
-  double volt = analogRead(0)*(5.0/1023.0); 
+  double volt = adc.read(0)*(5.0/1023.0); 
   double condVal = pow((50000-10000*volt)/volt,-1)*1e6; 
  
   //Print results w/ units
